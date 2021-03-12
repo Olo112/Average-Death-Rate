39db267bbfc440eedf351d92bdaabb8287890a5a
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-03-12 12:37:02 +0100

    adding debuggers

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@
 #   IMPORTS:
 #======================================================================
 
-from matplotlib import pyplot as plt
-
+from matplotlib import pyplot as plt        # for plots
+# import csv
 
 
 #======================================================================
@@ -13,7 +13,19 @@ from matplotlib import pyplot as plt
 ls_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]              # test lists
 ls_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
 
-file = 'file_name.txt'
+
+
+s_scv_file = 'file_name.txt'
+
+
+
+#---------------------------------------------------------
+#   DEBUGGERS:
+#---------------------------------------------------------
+
+_DBG0_ = True						# Errors
+_DBG1_ = True						# Warnings
+_DBG9_ = True						# Standard debug
 
 
 
@@ -23,20 +35,27 @@ file = 'file_name.txt'
 
 def find_data(file):
     '''
-    input:
-        file: file in which all the data is;
+        input:
+            file: file in which all the data is;
 
-    output:
-
+        output:
+            ls_data_01:     list of an average death rate for specific year(s);
     '''
 
-    global ls_data_01, ls_data_02
+    global ls_data_01  #ls_data_02
 
     ls_data_01 = []
-    ls_data_02 = []
+    #ls_data_02 = []
+
+    file_len = len(file)
+
+    # counting the sum of all elements in the list and adding to the list
+    element_sum = sum(file)
+
+    ls_data_01.append(element_sum)
 
 
-    return ls_data_01, ls_data_02
+    return ls_data_01   #ls_data_02
 
 
 
@@ -44,11 +63,21 @@ def find_data(file):
 #   MAIN:
 #======================================================================
 
-
 if (__name__ == '__main__'):
 
-    plt.plot(ls_1, ls_2)
+    # open the file and read it
+    #op_file = open(s_scv_file)
+
+
+    #rd_file = read
+
+    found_data = find_data(s_scv_file)
+
+    plt.plot(ls_1)
+    plt.grid()
     plt.show()              # displaying the plot
+
+
 
 #======================================================================
 #   END OF FILE
