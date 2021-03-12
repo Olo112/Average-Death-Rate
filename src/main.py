9e261c2a58132d298efee3dd7b9bf836c414eeb2
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-03-12 21:18:40 +0100

    displaying function example

--- src/main.py
+++ src/main.py
@@ -10,8 +10,13 @@ from matplotlib import pyplot as plt        # for plots
 #   GLOBALS:
 #======================================================================
 
-ls_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]              # test lists
 ls_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+
+ls_years = [1, 2, 3]
+
+ls_00 = [4442, 4545, 4666, 3992, 4333, 4567, 4862, 4532, 4091]
+ls_01 = [4444, 4555, 4866, 4992, 4339, 4537, 4962, 3532, 4092]
+ls_02 = [4440, 4595, 4066, 4692, 4634, 4687, 4762, 4932, 3091]
 
 
 
@@ -33,7 +38,7 @@ _DBG9_ = True						# Standard debug
 #   FUNCTIONS:
 #======================================================================
 
-def find_data(file):
+def find_data(ls_file):
     '''
         input:
             file: file in which all the data is;
@@ -42,17 +47,16 @@ def find_data(file):
             ls_data_01:     list of an average death rate for specific year(s);
     '''
 
-    global ls_data_01  #ls_data_02
+    global ls_data_01
 
     ls_data_01 = []
-    #ls_data_02 = []
 
-    file_len = len(file)
+    file_len = len(ls_file)
 
-    # counting the sum of all elements in the list and adding to the list
-    element_sum = sum(file)
+    element_sum = sum(ls_file)/file_len    # summing all elements and dividing it by file_len
 
     ls_data_01.append(element_sum)
+    if(_DBG9_): print(ls_data_01, '\nls_data_01 length =', file_len)
 
 
     return ls_data_01   #ls_data_02
@@ -70,10 +74,22 @@ if (__name__ == '__main__'):
 
 
     #rd_file = read
+    #found_data = find_data(s_scv_file)
 
-    found_data = find_data(s_scv_file)
+    ls_main_data = []
 
-    plt.plot(ls_1)
+    ls_data_00 = find_data(ls_00)
+    ls_data_01 = find_data(ls_01)
+    ls_data_02 = find_data(ls_02)
+
+    ls_main_data.append(ls_data_00[0])
+    ls_main_data.append(ls_data_01[0])
+    ls_main_data.append(ls_data_02[0])
+
+    plt.xlabel("years from 2015")
+    plt.ylabel("average deaths")
+
+    plt.plot(ls_years, ls_main_data, color='blue', marker='o')
     plt.grid()
     plt.show()              # displaying the plot
 
