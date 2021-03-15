a14c5b6c01768ba6ec346e29080c428067696122
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-03-15 13:38:39 +0100

    cleaning up the main python file

--- src/main.py
+++ src/main.py
@@ -10,17 +10,15 @@ from matplotlib import pyplot as plt        # for plots
 #   GLOBALS:
 #======================================================================
 
-ls_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
-
 ls_years = [1, 2, 3, 4]
 
-ls_00 = [4442, 4545, 4666, 3992, 4333, 4567, 4862, 4532, 4091]
+ls_00 = [4442, 4545, 4666, 3992, 4333, 4567, 4862, 4532, 4091]      # example lists
 ls_01 = [4444, 4555, 4866, 4992, 4339, 4537, 4962, 3532, 4092]
 ls_02 = [4440, 4595, 4066, 4692, 4634, 4687, 4762, 4932, 3091]
 ls_03 = [3077, 3471, 4244, 4956, 3834, 3844, 3844, 4611, 3285]
 
 
-s_scv_file = 'file_name.txt'
+s_scv_file = 'file_name.txt'        # example file name
 
 
 
@@ -50,9 +48,7 @@ def find_data(ls_file):
     global ls_data_01
 
     ls_data_01 = []
-
     file_len = len(ls_file)
-
     element_sum = sum(ls_file)/file_len    # summing all elements and dividing it by file_len
 
     ls_data_01.append(element_sum)
@@ -71,8 +67,6 @@ if (__name__ == '__main__'):
 
     # open the file and read it
     #op_file = open(s_scv_file)
-
-
     #rd_file = read
     #found_data = find_data(s_scv_file)
 
