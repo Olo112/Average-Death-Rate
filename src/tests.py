73a01b84e811e52cb7381cf4d36d50287233daa9
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-05-10 15:57:16 +0200

    adding debuggers

--- src/tests.py
+++ src/tests.py
@@ -8,6 +8,16 @@ import count as c
 
 
 
+#---------------------------------------------------------
+#   DEBUGGERS:
+#---------------------------------------------------------
+
+_DBG0_ = True						# Errors
+_DBG1_ = True						# Warnings
+_DBG9_ = True						# Standard debug
+
+
+
 #======================================================================
 #   MAIN TESTS:
 #======================================================================
@@ -19,18 +29,21 @@ if (__name__ == '__main__'):
     file = open('liczba_zgonow_w_rejestrze_od_1_wrzesnia_2015.csv', 'r')
     reader = csv.reader(file)
 
+    print("reader: \n")
+
+    if (_DBG9_):
+        for i in reader:
+            print(i)
+
+
     test_operations = c.Operations()     # creating main object
 
 
     summed = test_operations.sum_data_columns(12)
     print(summed, '\n\n')
 
-    fixed_csv = test_operations.fix_csv(csv_file='liczba_zgonow_w_rejestrze_od_1_wrzesnia_2015.csv', col_num=0)   # deleting first column and row
-
-    data = reader
-
-    for i in data:
-        print(i)
+    fixed_data = test_operations.fix_csv(csv_file='liczba_zgonow_w_rejestrze_od_1_wrzesnia_2015.csv', col_num=0)   # deleting first column and row
+    #print()
 
 
         # filling blank with 0 digit:
