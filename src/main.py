9dec865c95d6437d074350527c9a5e0c1ccca149
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-05-18 15:18:17 +0200

    finishing plotting

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 #======================================================================
 #   IMPORTS:
 #======================================================================
-from typing import Type
 
 from matplotlib import pyplot as plt        # for plots
 import csv                                  # for operations on csv file(s)
@@ -32,27 +31,77 @@ _DBG9_ = True						# Standard debug
 #======================================================================
 
 if (__name__ == '__main__'):
-    count_data = c.Operations()   # creating main object
+
+
+    data_counter = c.Operations()   # creating main object
 
     main_file = open(s_scv_file)
-    reader = csv.reader(main_file)      # csv reader to read all data
+    reader = csv.reader(main_file)      # creating a csv reader to read all data
 
-    ls_fixed_data = count_data.fix_csv(csv_file=s_scv_file, col_num=0)     # fix the main data (deleting first column)
+    ls_float_data = []
+
+
+
+    ls_fixed_data = data_counter.fix_csv(csv_file=s_scv_file, col_num=0)     # fix the main data (deleting first column)
     if (_DBG9_):
         for row in ls_fixed_data:
             print(row)
 
-
-    # filling blank with 0 digit
-
-
-    # converting all data vaules to float
+        print('\n\n')
 
 
-    # deleting years list ls_filled_float_data[0]
+    ls_filled_data = data_counter.zero_to_blank(ls_fixed_data)   # filling blank with 0 digit
+    if (_DBG9_):
+        for row in ls_filled_data:
+            print(row)
+
+        print('\n\n')
 
 
-    # summing columns and
+    for row in ls_filled_data:
+        row = [float(data_val) for data_val in row]     # converting all data vaules to float
+        ls_float_data.append(row)
+
+
+        if (_DBG9_): print(row)
+
+    print('\n\n')
+
+
+    del ls_float_data[0]       # deleting years list ls_filled_float_data[0]
+
+    if (_DBG9_):
+        for row in ls_float_data:
+            print(row)
+        print('\n\n')
+
+
+    ls_years = []
+    year = 2015
+    for i in range(len(ls_float_data[0])):
+        ls_years.append(year)
+        year = year + 1
+
+    if (_DBG9_): print('years =', ls_years, '\n\n')
+
+
+    ls_main_data = data_counter.sum_data_columns(ls_float_data)     # going through columns and counting average
+
+
+
+    # displaying the data on graph (creating plots):
+
+    # 1. naming labels of OX and OY:
+    plt.xlabel('years from 2015')      # OX label: years from 2015
+    plt.ylabel('ADR')                  # OY label: ADR (short: average death rate)
+
+
+    # 2. adding plot:
+    plt.plot(ls_years, ls_main_data, label='ADR', marker='o')      # OX data, OY data,
+
+    # 2. displaying (with grid):
+    #plt.grid(True)
+    #plt.show()
 
 
 #======================================================================
